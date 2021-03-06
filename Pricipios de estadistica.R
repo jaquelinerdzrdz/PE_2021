7ee84c7869a8649cb0c7a966368c14c3d24c01ba
jaquelinerdzrdz <78894360+jaquelinerdzrdz@users.noreply.github.com>
2021-03-06 15:30:41 -0600

    Update Lab

---
+++ Pricipios de estadistica.R
@@ -0,0 +1,14 @@
+# Nallely Jaqueline Rodriguez Rodriguez
+# Matricula 1854081
+# Fecha: 22.02.2021
+
+# Superficie
+3110 + 1453 + 450 + 1200 + 720 
+Pinus <- 3140
+Mezquite <- 1453
+Encinos <- 450
+Teka <- 1200
+Juniperos <- 720
+Superficie <- c(Pinus, Mezquite, Encinos, Teka, Juniperos)
+barplot(Superficie)
+sort(Superficie)
---
+++ Principios de Estadistica.R
@@ -0,0 +1,8 @@
+# Nallely Jaqueline Rodriguez Rodriguez
+# Matricula 1854081
+# Fecha: 22.02.2021
+
+# Altura de planta Prosopis 
+altura <- c(38, 14, 44, 11, 9, 21, 19, 28, 41, 4, 35, 24, 36, 12, 20, 31, 24, 25, 10, 21, 11, 36, 37, 20, 26)
+mean(altura)
+sd(altura)
